dbcaf71578d7cf98a948578cdf9e7d8253348aa8
Romain Durand <romain.durand@radiofrance.com>
2024-10-18 17:54:34 +0200

    implement reactivity for all boolean map options

--- src/lib/updateProps.ts
+++ src/lib/updateProps.ts
@@ -1,18 +1,119 @@
-import type { Map, Marker } from 'leaflet';
+import type { Map, MapOptions, Marker } from 'leaflet';
 
-export function updateProps(instance: Map | Marker, prevProps: SvelteAllProps) {
+export function updateProps<T extends Map | Marker>(instance: T, newProps: AnyLeafletInstance) {
 	const props = instance.options;
 
-	// iterate through the previous props
-	// if the prop is in the new props and the value is different
+	// iterate through the new props
+	// if the prop is in the old props and the value is different
 	// update the instance options using the instance setter
-	for (const [key, value] of Object.entries(prevProps)) {
-		if (key in props && props[key] !== value) {
-			instance[`set${capitalize(key)}`](value);
+	for (const [key, value] of Object.entries(newProps)) {
+		if (key in props && (props as Record<string, unknown>)[key] !== value) {
+			const setterName = `set${capitalize(key)}`;
+			if (!(setterName in instance)) {
+				console.error(`Invalid prop ${key}`);
+				continue;
+			}
+			const setter = (instance as unknown as Record<string, (v: unknown) => unknown>)[setterName];
+			setter(value);
 		}
 	}
 }
 
 function capitalize(str: string) {
 	return str[0].toUpperCase() + str.slice(1);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type AnyLeafletInstance = { [key: string]: any };
+
+// stores the function bound to the event listener so it can be removed later
+let boundInvalidateMapSize: null | (() => void) = null;
+function invalidateMapSize(map: Map) {
+	map.invalidateSize({
+		// TODO : add a way to customize these default values
+		debounceMoveend: true,
+		pan: true
+	});
+}
+export function updateMapProps(L: typeof import('leaflet'), map: Map, newProps: MapOptions) {
+	if (!newProps) return;
+	for (const [key, value] of Object.entries(newProps)) {
+		// skip if the option value is unchanged
+		if (map.options[key as keyof MapOptions] === value) continue;
+		switch (key) {
+			// TODO : move check of unsupported options before the unchanged check
+			case 'fadeAnimation':
+			case 'zoomAnimation':
+				// some could be hacked around by changing style attributes
+				// not a priority
+				throw new Error(`mutation of ${key} option is not supported`);
+
+			// enable/disable cases (handlers)
+			case 'boxZoom':
+			case 'scrollWheelZoom':
+			case 'doubleClickZoom':
+			case 'dragging':
+			case 'keyboard':
+			case 'tapHold':
+			case 'touchZoom': // untested
+				map.options[key as keyof MapOptions] = value;
+				if (value) map[key].enable();
+				else map[key].disable();
+				break;
+
+			// simple cases
+			case 'inertia':
+			case 'preferCanvas':
+			case 'bounceAtZoomLimits':
+			case 'markerZoomAnimation': // untested
+			case 'worldCopyJump': // untested
+			case 'closePopupOnClick': // untested
+				map.options[key] = value;
+				break;
+
+			// Control cases
+			case 'zoomControl':
+				if (value) {
+					map.zoomControl = map.zoomControl || L.control.zoom();
+					map.zoomControl.addTo(map);
+				} else map.zoomControl?.remove();
+				break;
+			case 'attributionControl':
+				if (value) {
+					map.attributionControl = map.attributionControl || L.control.attribution();
+					map.attributionControl.addTo(map);
+				} else map.attributionControl?.remove();
+				break;
+
+			//complex cases
+			case 'trackResize':
+				if (!boundInvalidateMapSize) boundInvalidateMapSize = invalidateMapSize.bind(null, map);
+				window.removeEventListener('resize', boundInvalidateMapSize);
+				if (value) {
+					window.addEventListener('resize', boundInvalidateMapSize);
+					invalidateMapSize(map);
+				}
+				break;
+		}
+	}
+}
+export type BooleanMapOption = keyof {
+	[K in keyof MapOptions as true extends UnionContainsBoolean<MapOptions[K]>
+		? K
+		: never]: MapOptions[K];
+};
+
+type UnionContainsBoolean<T> = T extends boolean ? true : never;
+
+// TODO : remove when https://github.com/DefinitelyTyped/DefinitelyTyped/pull/70943 is merged and published
+declare module 'leaflet' {
+	interface MapOptions {
+		tapHold?: boolean;
+	}
+	interface Map {
+		tapHold: {
+			enable(): void;
+			disable(): void;
+		};
+	}
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,64 @@
 <script lang="ts">
 	import '../global.css';
-	import type { LeafletMap, LatLngExpression } from '$lib/index.js';
+	import type { LeafletMap, LatLngExpression, MapOptions } from '$lib/index.js';
 	import Map from '$lib/Map.svelte';
+	import type { BooleanMapOption } from '$lib/updateProps.js';
 
 	let map: LeafletMap | undefined = $state();
 	const initialView: LatLngExpression = [48.86750658335676, 2.3638381549875467];
-	let draggable = $state(true);
+	let options: MapOptions = $state({
+		preferCanvas: false,
+		attributionControl: false,
+		zoomControl: true,
+		closePopupOnClick: true,
+		trackResize: true,
+		boxZoom: true,
+		doubleClickZoom: true,
+		dragging: true,
+		fadeAnimation: true,
+		markerZoomAnimation: true,
+		zoomAnimation: true,
+		inertia: true,
+		worldCopyJump: false,
+		keyboard: true,
+		scrollWheelZoom: true,
+		tapHold: true,
+		touchZoom: true,
+		bounceAtZoomLimits: true,
+
+		center: initialView,
+		zoom: 18
+	});
+
+	const booleanOptions = Object.keys(options).filter(
+		(key) => typeof options[key as keyof typeof options] === 'boolean'
+	) as BooleanMapOption[];
 </script>
 
 <Map
 	focusable={false}
-	options={{ center: initialView, zoom: 18, dragging: draggable }}
+	{options}
 	bind:instance={map}
-	onmousemove={(e) => {
-		console.log({ e });
-	}}
+	oncontextmenu={() => console.log('contextmenu')}
 />
+
+<div class="controls">
+	{#each booleanOptions as key}
+		<button onclick={() => (options[key] = !options[key])}>{key}: {options[key]}</button>
+	{/each}
+</div>
+
+<style>
+	.controls {
+		border: 1px solid grey;
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+		position: absolute;
+		top: 0;
+		right: 0;
+		z-index: 1000;
+		background: white;
+		padding: 0.5em;
+	}
+</style>
