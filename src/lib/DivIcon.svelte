334f15e3e138195fe1e50bd08637d5aa3128d53b
Romain Durand <romain.durand@radiofrance.com>
2024-10-25 09:12:46 +0200

    DivIcon + Marker refactoring

---
+++ src/lib/DivIcon.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { getContext, onMount, tick, type Snippet } from 'svelte';
+	import type { Marker as LeafletMarker } from 'leaflet';
+	type Props = { children: Snippet };
+
+	let { children }: Props = $props();
+	let iconContainer: HTMLDivElement | undefined = $state();
+
+	const getMarker = getContext<() => LeafletMarker>('marker');
+	const L = globalThis.window.L;
+
+	onMount(async () => {
+		await tick();
+		const marker = getMarker?.();
+		if (marker) {
+			const divIcon = L.divIcon({
+				html: iconContainer,
+				className: ''
+			});
+			marker.setIcon(divIcon);
+		}
+	});
+</script>
+
+<div class="DivIcon" bind:this={iconContainer}>
+	{@render children()}
+</div>
