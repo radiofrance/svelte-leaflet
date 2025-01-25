d3395496cce71fb9733f9c7cb2524b20ef94aec4
Vivian SARAZIN <9358572+vvnsrzn@users.noreply.github.com>
2025-01-25 21:25:12 +0100

    refactor: to add an alias in order to ease the routes folder

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,11 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
+		alias: {
+			$components: './src/components'
+		}
 	},
+
 	extensions: ['.svelte', '.svx'],
 };
 
